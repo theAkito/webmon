d23486f6ada2ccf47e32455468389333571538c9
Akito <the@akito.ooo>
2021-10-10 18:58:07 +0200

    Aggregate multiple offline Websites into a single Notification

--- app/src/main/java/com/manimarank/websitemonitor/worker/SyncWorker.kt
+++ app/src/main/java/com/manimarank/websitemonitor/worker/SyncWorker.kt
@@ -4,10 +4,11 @@ package com.manimarank.websitemonitor.worker
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.manimarank.websitemonitor.R
 import com.manimarank.websitemonitor.data.repository.WebSiteEntryRepository
 import com.manimarank.websitemonitor.utils.Print
 import com.manimarank.websitemonitor.utils.Utils
+import com.manimarank.websitemonitor.utils.Utils.getStringNotWorking
+import com.manimarank.websitemonitor.utils.Utils.joinToStringDescription
 
 
 class SyncWorker(appContext: Context, workerParams: WorkerParameters) :
@@ -15,17 +16,39 @@ class SyncWorker(appContext: Context, workerParams: WorkerParameters) :
     private lateinit var repository: WebSiteEntryRepository
 
     override suspend fun doWork(): Result {
+        /*
+          Running according to set interval.
+          Shows Notifications for failed Websites, when App is in Background.
+        */
+
+        if (Utils.appIsVisible()) {
+            /*
+              We only want to send notifications, when App is in Background.
+            */
+            return Result.success()
+        }
+
         val applicationContext: Context = applicationContext
 
         repository = WebSiteEntryRepository(applicationContext)
 
-        Print.log("Fetching Data from Remote host")
+        Print.log("Fetching Data from Remote hosts...")
         return try {
-            repository.checkWebSiteStatus().filter { Utils.isValidNotifyStatus(it.status) }.forEach {
-                Print.log("Error Page : $it")
-                if (Utils.appIsVisible().not()) {
-                    Utils.showNotification(applicationContext, it.name, String.format(applicationContext.getString(R.string.not_working, it.url)))
-                }
+            val entriesWithFailedConnection =
+                repository.checkWebSiteStatus().filter { Utils.mayNotifyStatusFailure(it.status) }
+            if (entriesWithFailedConnection.size == 1) {
+                val entryWithFailedConnection = entriesWithFailedConnection.first()
+                Utils.showNotification(
+                    applicationContext,
+                    entryWithFailedConnection.name,
+                    applicationContext.getStringNotWorking(entryWithFailedConnection.url)
+                )
+            } else {
+                Utils.showNotification(
+                    applicationContext,
+                    "Several Websites are not reachable!",
+                    entriesWithFailedConnection.joinToStringDescription()
+                )
             }
             Result.success()
         } catch (e: Throwable) {
