9b8e5f6b102b524cc015e1b277cb8d4909ec99f2
Akito <the@akito.ooo>
2021-11-03 01:33:56 +0100

    Improve Logger

---
+++ app/src/main/kotlin/ooo/akito/webmon/utils/Log.kt
@@ -0,0 +1,18 @@
+package ooo.akito.webmon.utils
+
+import android.util.Log
+import ooo.akito.webmon.utils.Constants.TAG_GLOBAL
+
+object Log {
+  fun error(msg : String) {
+    Log.e(TAG_GLOBAL, msg)
+  }
+
+  fun warn(msg : String) {
+    Log.w(TAG_GLOBAL, msg)
+  }
+
+  fun info(msg : String) {
+    Log.i(TAG_GLOBAL, msg)
+  }
+}
