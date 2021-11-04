49a05e1239aa859cca161c4dea5f6ee547543545
Akito <the@akito.ooo>
2021-11-04 03:03:10 +0100

    Add Possibility to check for successful Connection to Onion Address

--- app/src/main/kotlin/ooo/akito/webmon/net/Utils.kt
+++ app/src/main/kotlin/ooo/akito/webmon/net/Utils.kt
@@ -1,5 +1,6 @@
 package ooo.akito.webmon.net
 
+import ooo.akito.webmon.net.proxy.ProxyProvider
 import ooo.akito.webmon.utils.ArrayIP
 
 object Utils {
@@ -9,5 +10,13 @@ object Utils {
   fun List<ArrayIP>.normaliseToString(): List<String> = this.map { it.normaliseToString() }
   fun List<ByteArray>.normaliseToUShort(): List<ArrayIP> = this.map { it.map { (it.toInt() and 0xFF).toUShort() }.toUShortArray() }
   fun List<ByteArray>.normaliseToString(): List<String> = this.normaliseToUShort().normaliseToString()
+
+  fun onionConnectionIsSuccessful(onionUrl: String): Boolean {
+    // Example Onion URI: "duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion"
+    val conn = ProxyProvider.connectSocks4a(onionUrl, 80, "127.0.0.1", 9050)
+    val success = conn.isConnected
+    conn.close()
+    return success
+  }
 
 }
