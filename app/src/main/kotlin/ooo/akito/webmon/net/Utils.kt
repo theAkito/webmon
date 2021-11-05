377f08b954fd975de9f5d8aecf912d0176c1de17
Akito <the@akito.ooo>
2021-11-05 17:29:16 +0100

    Overhaul DNS Resolution & Handle IPv6

--- app/src/main/kotlin/ooo/akito/webmon/net/Utils.kt
+++ app/src/main/kotlin/ooo/akito/webmon/net/Utils.kt
@@ -1,16 +1,9 @@
 package ooo.akito.webmon.net
 
 import ooo.akito.webmon.net.proxy.ProxyProvider
-import ooo.akito.webmon.utils.ArrayIP
 import ooo.akito.webmon.utils.Log
 
 object Utils {
-
-  private fun ArrayIP.normaliseToString(): String = this.joinToString(".")
-  @JvmName("normaliseToStringArrayIP")
-  fun List<ArrayIP>.normaliseToString(): List<String> = this.map { it.normaliseToString() }
-  fun List<ByteArray>.normaliseToUShort(): List<ArrayIP> = this.map { it.map { (it.toInt() and 0xFF).toUShort() }.toUShortArray() }
-  fun List<ByteArray>.normaliseToString(): List<String> = this.normaliseToUShort().normaliseToString()
 
   fun onionConnectionIsSuccessful(onionUrl: String): Boolean {
     // Example Onion URI: "duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion"
--- app/src/main/kotlin/ooo/akito/webmon/net/dns/DNS.kt
+++ app/src/main/kotlin/ooo/akito/webmon/net/dns/DNS.kt
@@ -1,6 +1,5 @@
 package ooo.akito.webmon.net.dns
 
-import ooo.akito.webmon.net.Utils.normaliseToString
 import ooo.akito.webmon.utils.ExceptionCompanion.msgDnsOnlyNXDOMAIN
 import org.minidns.dnsmessage.DnsMessage
 import org.minidns.hla.ResolverApi
@@ -8,8 +7,6 @@ import org.minidns.hla.ResolverResult
 import org.minidns.record.A
 import org.minidns.record.AAAA
 import org.minidns.record.Data
-import org.minidns.record.InternetAddressRR
-import java.net.InetAddress
 
 class DNS {
 
@@ -19,17 +16,20 @@ class DNS {
     return this.all { it.responseCode.value == DnsMessage.RESPONSE_CODE.NX_DOMAIN.value }
   }
 
-  fun retrieveAllIPsFromDnsRecords(url: String): List<ByteArray> {
+  fun retrieveAllIPsFromDnsRecords(url: String): List<String> {
     /** A Records */
     val resultA: ResolverResult<A> = ResolverApi.INSTANCE.resolve(url, A::class.java)
+
     /** AAAA Records */
     val resultAAAA: ResolverResult<AAAA> = ResolverApi.INSTANCE.resolve(url, AAAA::class.java)
+
     /** A Records' Answers */
     val answersA = try {
       (resultA.answers as Set<*>).filterIsInstance<A>()
     } catch (e: Exception) {
       emptyList()
     }
+
     /** AAAA Records' Answers */
     val answersAAAA = try {
       (resultAAAA.answers as Set<*>).filterIsInstance<AAAA>()
@@ -38,13 +38,11 @@ class DNS {
     }
 
     val resultData: List<ResolverResult<out Data>> = listOf(resultA, resultAAAA)
-    if (resultData.areNXDOMAIN()) { throw ResolvesToNowhereException(msgDnsOnlyNXDOMAIN) }
+    if (resultData.areNXDOMAIN()) {
+      throw ResolvesToNowhereException(msgDnsOnlyNXDOMAIN)
+    }
 
-    val answersAll: List<InternetAddressRR<out InetAddress>> = answersA + answersAAAA
-
-    return answersAll.mapNotNull { it.ip }
+    return answersA.mapNotNull { it.inetAddress.hostAddress } + answersAAAA.mapNotNull { aaaa -> aaaa.inetAddress.hostAddress?.let { "[${it}]" } }
   }
-
-  fun retrieveAllIPsFromDnsRecordsAsStrings(url: String): List<String> = retrieveAllIPsFromDnsRecords(url).normaliseToString()
 
 }
